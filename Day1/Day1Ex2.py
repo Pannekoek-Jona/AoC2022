4afb77140c79833c68ea1379fdc5bdda05a2f109
Hukseflux - Jonathan VAN DE REEP <jree@huksefluxts.local>
2022-12-01 13:26:06 +0100

    Day1Ex2 finished

--- Day1/Day1Ex2.py
+++ Day1/Day1Ex2.py
@@ -1,13 +1,15 @@
 if __name__ == "__main__":
     docLines = []
     SubTotal = 0
-    HighScore = []
+    HighScore = [0, 0, 0]
     with open('inputday1.txt') as my_file:
         for line in my_file:
             if line != '\n':
                 SubTotal += int(line)
             else:
-                if SubTotal > HighScore:
-                    HighScore = SubTotal
+                if SubTotal > HighScore[0]:
+                    HighScore[0] = SubTotal
+                    HighScore.sort()
                 SubTotal = 0
-    print(HighScore)
+    print(HighScore[0], HighScore[1], HighScore[2])
+    print(HighScore[0] + HighScore[1] + HighScore[2])
